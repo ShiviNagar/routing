2c49e2a85843ddf988588dd176d4531d6a101b84
your name <you@example.com>
2020-03-26 00:15:06 +0530

    first commit

---
+++ src/app/some.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SomeService {
+
+  constructor(private http:HttpClient) { }
+  getData(){
+    this.http.get(this.Url)
+  }
+}
